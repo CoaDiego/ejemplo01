836dbe3ff2f48d608bcbfcb5f7a69af5d12d43ef
Diego Coa <coadiego20@gmail.com>
2020-11-25 19:10:00 +0100

    Agregar archivos de proyecto.

---
+++ PRÁCTICO 01/principal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Nombre: Coa Veliz Diego Armando" << endl;
+	cout << "Carrera: Ing.Sistemas" << endl;
+	cout << "Ci: 5632247, Cu:35-4612" << endl;
+
+	//Ingeniero disculpe, pero hasta este momento pude "reparar" mi PC y es que ha estado funcionando mal estos dias y no podia hacer uso de ella.
+	system("pause");
+
+	return 0;
+
+}
